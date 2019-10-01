aff422b2df3fb92b0b8cb2f2110043d2e6f7d314
Dimitris Chrysomallis <dxrysom@gmail.com>
2019-10-01 22:14:42 +0300

    Change build css script.

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@ echo Building css
 
 npx tailwind build ./src/assets/css/styles.css -c ./tailwind.config.js -o ./src/_site/assets/css/build.css 
 
-echo build.css is $(du -sh src/_site/assets/css/build.css | cut -f 1)
+echo build.css is $(du -sh ./src/_site/assets/css/build.css | cut -f 1)
 
 echo Purging css
 
-npx purgecss -c purgecss.config.js -o src/_site/assets/css
+npx purgecss -c purgecss.config.js -o ./src/_site/assets/css
 
-echo build.css is $(du -sh src/_site/assets/css/build.css | cut -f 1)
+echo build.css is $(du -sh ./src/_site/assets/css/build.css | cut -f 1)
